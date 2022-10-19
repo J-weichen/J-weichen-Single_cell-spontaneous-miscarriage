07316697a5c8d727a5f4d804bbe8583927566a68
WeiChen <54924112+J-weichen@users.noreply.github.com>
2022-10-19 23:41:42 +0800

    Delete scRNA directory

--- scRNA/figure_3_s2_cmd.sh
+++
@@ -1,2 +0,0 @@
-sh figure_three_step_2_runPySCENIC.sh /mnt/data/chenwei/gongchen/4.SCENIC/loom_result  ALLcell_DEGs_selected
-#sh figure_three_step_2_runPySCENIC.sh /mnt/data/chenwei/gongchen/4.SCENIC/loom_result  ALLcell_all_genes
